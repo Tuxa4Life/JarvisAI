cea77f4b915d8b2629493dd0b0c0836d02535dfa
Tuxa4Life <nikoloztuxa@gmail.com>
2022-11-13 16:06:06 +0400

    Commit

--- main.py
+++ main.py
@@ -15,50 +15,19 @@ def talk(text):
     engine.say(text)
     engine.runAndWait()
 
-
-def take_command():
+def speak():
     try:
-        with sr.Microphone() as source:
-            print('listening...')
-            voice = listener.listen(source)
+        with sr.Microphone() as mic:
+            print('I\'m listening')
+            voice = listener.listen(mic)
             command = listener.recognize_google(voice)
             command = command.lower()
-            if 'jarvis' in command or 'gervais' in command or 'service' in command:
+            if 'jarvis' in command:
                 command = command.replace('jarvis', '')
-                command = command.replace('gervais', '')
-                command = command.replace('service', '')
-                print(command)
-    except:
-        pass
-    return command
+                return command
 
+    except NameError:
+        talk('I couldn\'t hear you')
+        print(NameError)
 
-running = True
-def run_alexa():
-    command = take_command()
-    print(command)
-    if 'play' in command:
-        song = command.replace('play', '')
-        talk('playing ' + song)
-        pywhatkit.playonyt(song)
-    elif 'time' in command:
-        time = datetime.datetime.now().strftime('%I:%M %p')
-        talk('Current time is ' + time)
-    elif 'wikipedia' in command:
-        person = command.replace('wikipedia', '')
-        info = wikipedia.summary(person, 1)
-        print(info)
-        talk(info)
-    elif 'date' in command:
-        talk('Today is' + str(datetime.date.today()))
-    elif 'joke' in command:
-        talk(pyjokes.get_joke())
-    elif 'turn off':
-        global running
-        running = False
-    else:
-        talk('Please say the command again.')
-
-
-while running:
-    run_alexa()
+print(speak())
